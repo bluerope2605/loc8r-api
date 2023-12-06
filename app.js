2879112c50eb06b1a1d5768eb117ad4f98d76b09
bluerope2605 <149867966+bluerope2605@users.noreply.github.com>
2023-12-07 06:53:27 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const passport = require('passport');
 require('./app_api/models/db');
+require('./app_api/config/passport');
 
-const indexRouter = require('./app_server/routes/index');
-const apiRouter = require('./app_api/routes/index');
+// var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var apiRouter = require('./app_api/routes/index');
 
-const app = express();
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -19,9 +23,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
+app.use(passport.initialize());
 
-app.use('/', indexRouter);
+app.use('/api', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", 'http://localhost:4200');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-type, Accept, Authorization");
+  next();
+});
+
+// app.use('/', indexRouter);
 app.use('/api', apiRouter);
+app.use('/users', usersRouter);
+
+// app.get('*', function(req, res, next) {
+//   res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
+// });
+
+app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  if(err.name === 'UnauthorizedError'){
+    res.status(401).json({"message":err.name + ": " +err.message});
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,7 +56,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,5 +65,6 @@ app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
